da0b5d118dffff90362cce5bce45d4d745f724ca
KatsuKira <christopher.caysido@gmail.com>
2024-07-22 16:22:30 +0800

    Created initial stockpicker method and get_lowest_day method
    get_lowest_day method returns the index of the lowest value of the array

--- stockpicker.rb
+++ stockpicker.rb
@@ -6,3 +6,28 @@
 #   return Array.new
 # }
 
+def stockpicker(stock_prices)
+  # Initialize the best days arrat
+  best_days = Array.new
+  # Check which day is the lowest
+  lowest_day = get_lowest_day(stock_prices)
+end
+
+def get_lowest_day(stock_prices)
+  debugger
+  value = stock_prices.reduce do |lowest_value, stock_price|
+    if stock_price < lowest_value
+      lowest_value = stock_price
+    else
+      lowest_value = lowest_value
+    end
+    lowest_value
+  end
+
+  # Get index
+  stock_prices.find_index(value)
+end
+
+stock_prices = [17,3,6,9,15,8,6,1,10]
+
+stockpicker(stock_prices)
