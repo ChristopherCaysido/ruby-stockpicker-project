5b9340afe6fc1e8a2245fd975de3d88ab737d62a
KatsuKira <christopher.caysido@gmail.com>
2024-07-24 19:42:33 +0800

    Created the get_keys and get_index functions

--- stockpicker.rb
+++ stockpicker.rb
@@ -11,9 +11,11 @@
 # Need to transform the array into hashes?
 
 
-# def stockpicker(stock_prices)
-  
-# end
+def stockpicker(stock_prices)
+  combination_hash = combinations(stock_prices)
+  stock_item = get_keys(combination_hash)
+  get_index(stock_item)
+end
 
 def combinations(stock_prices)
   # Create a new hash to store the combination of numbers 
@@ -32,6 +34,32 @@ def combinations(stock_prices)
   hash
 end
 
+def get_keys(stock_hash)
+  keys = []  
+  stock_hash.each do |key, value|
+    keys.push(key)
+  end
+  stock_hash
+  filtered_stock_prices = keys.reduce(Array.new) do |acc, key_stock_hash|
+    acc.push(stock_hash[key_stock_hash][:prices])
+    acc
+  end
+  max_value = filtered_stock_prices.max
+  stock_item =  stock_hash.select do |key, value|
+    stock_hash[key][:prices] == 12
+  end 
+  return stock_item
+end
+
+def get_index(stock_item)
+  indices = stock_item.reduce do |item_index, (key, value)|
+    p stock_item[key][:index]
+    item_index.push(stock_item[key][:index])
+    item_index
+  end
+  return indices
+end
+
 stock_prices = [17,3,6,9,15,8,6,1,10]
 
-puts combinations(stock_prices)
+p stockpicker(stock_prices)
